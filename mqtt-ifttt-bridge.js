625f4c8f88e28d614aa500fe2b63e4d4bd3f9197
Justin Wood <woody@apple.com>
2017-04-18 16:07:14 -0500

    Working version

--- mqtt-ifttt-bridge.js
+++ mqtt-ifttt-bridge.js
@@ -1,6 +1,7 @@
 // Requirements
 mqtt = require('mqtt')
-
+url = require('url')
+express = require('express')
 logging = require('./homeautomation-js-lib/logging.js')
 mqtt_helpers = require('./homeautomation-js-lib/mqtt_helpers.js')
 
@@ -9,7 +10,7 @@ mqtt_helpers = require('./homeautomation-js-lib/mqtt_helpers.js')
 host = process.env.MQTT_HOST
 
 // Set up modules
-logging.set_enabled(fatruelse)
+logging.set_enabled(true)
 
 // Setup MQTT
 client = mqtt.connect(host)
@@ -28,4 +29,21 @@ client.on('disconnect', () => {
 
 client.on('message', (topic, message) => {
     logging.log(" " + topic + ":" + message)
+})
+
+// Web front end
+app = express()
+
+app.get('/ifttt/*', function(req, res) {
+    url_info = url.parse(req.url, true)
+    topic = url_info.pathname.slice(6)
+    value = url_info.query.value
+
+    logging.log("Publishing: " + topic + ":" + value)
+    mqtt_helpers.publish(client, topic, value)
+    res.send('topic: ' + topic + " value: " + value)
+})
+
+app.listen(3000, function() {
+    console.log('IFTTT listener started on port 3000')
 })
