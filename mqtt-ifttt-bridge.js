59fbc196fac89ab43c66f0015e4d903ac45aac0e
Justin Wood <woody@apple.com>
2017-12-02 14:55:22 -0800

    Removing smart publish

--- mqtt-ifttt-bridge.js
+++ mqtt-ifttt-bridge.js
@@ -4,6 +4,7 @@ const url = require('url')
 const express = require('express')
 const bodyParser = require('body-parser');
 const logging = require('homeautomation-js-lib/logging.js')
+const _ = request('lodash')
 
 require('homeautomation-js-lib/mqtt_helpers.js')
 
@@ -19,22 +20,37 @@ app.use(bodyParser);
 
 app.post('/ifttt/post', function(req, res) {
     const body = req.body
+    if (_.isNil(body) || _.isNil(body.topic)) {
+        res.send('Body has empty topic: ' + JSON.stringify(body))
+        return
+    }
+
+    if (_.isNil(body) || _.isNil(body.value)) {
+        res.send('Body has empty value: ' + JSON.stringify(body))
+        return
+    }
 
     var topic = body.topic
     var value = body.value
 
     logging.log('Publishing: ' + topic + ':' + value)
-    client.smartPublish(topic, value)
+    client.publish(topic, value)
     res.send('topic: ' + topic + ' value: ' + value)
 })
 
 app.get('/ifttt/*', function(req, res) {
     var url_info = url.parse(req.url, true)
+
+    if (_.isNil(req.url) || _.isNil(url_info)) {
+        res.send('Empty URL')
+        return
+    }
+
     var topic = url_info.pathname.slice(6)
     var value = url_info.query.value
 
     logging.log('Publishing: ' + topic + ':' + value)
-    client.smartPublish(topic, value)
+    client.publish(topic, value)
     res.send('topic: ' + topic + ' value: ' + value)
 })
 
