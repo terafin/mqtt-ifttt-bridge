b017383fe7ed01b9b706d474a94b2aca8630fa3b
Justin Wood <woody@apple.com>
2017-12-02 14:52:11 -0800

    Adding post

--- mqtt-ifttt-bridge.js
+++ mqtt-ifttt-bridge.js
@@ -2,7 +2,9 @@
 const mqtt = require('mqtt')
 const url = require('url')
 const express = require('express')
+const bodyParser = require('body-parser');
 const logging = require('homeautomation-js-lib/logging.js')
+
 require('homeautomation-js-lib/mqtt_helpers.js')
 
 
@@ -11,6 +13,20 @@ var client = mqtt.setupClient(null, null)
 
 // Web front end
 var app = express()
+app.use(bodyParser);
+
+app.use(bodyParser);
+
+app.post('/ifttt/post', function(req, res) {
+    const body = req.body
+
+    var topic = body.topic
+    var value = body.value
+
+    logging.log('Publishing: ' + topic + ':' + value)
+    client.smartPublish(topic, value)
+    res.send('topic: ' + topic + ' value: ' + value)
+})
 
 app.get('/ifttt/*', function(req, res) {
     var url_info = url.parse(req.url, true)
